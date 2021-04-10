dd541f4cc7a995f3f548fde1e7f8ab2b34f08b81
Isra1997 <israragheb@gmail.com>
2021-04-10 11:20:47 +0200

    added:file-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,3 @@
 import { handleSubmit } from './js/formHandler'
+import img from './js/download.png'
 alert("I EXIST")
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
                 test : '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },{
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: "file-loader"
             }
         ]
     }
